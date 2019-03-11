0244abaaa3b1088963399de77a76311f7fb266ea
sunyuecheng <sunyuecheng@sina.cn>
2019-03-11 14:13:41 +0800

    Commit alps sample

---
+++ alps/modules/alps-module-console/src/test/java/com/projn/sample/alps/module/console/util/VerificationCodeUtilsTest.java
@@ -0,0 +1,11 @@
+package com.projn.sample.alps.module.console.util;
+
+import org.junit.Test;
+
+public class VerificationCodeUtilsTest {
+    @Test
+    public void generateCodeAndPicture() throws Exception {
+
+    }
+
+}
